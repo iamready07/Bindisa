468c4ec8fa4d146eddd01dbd22690a01bd1f67a7
Aditya Deshmukh <156204127+iamready07@users.noreply.github.com>
2025-07-01 03:26:18 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,18 @@ import Footer from "./components/Footer";
 
 // Pages
 import Home from "./pages/Home";
-
+import About from "./pages/About";
+import Team from "./pages/Team";
+import Technology from "./pages/Technology";
+import SuccessStories from "./pages/SuccessStories";
+import SoilAnalysis from "./pages/SoilAnalysis";
+import Chatbot from "./pages/Chatbot";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Contact from "./pages/Contact";
+import Location from "./pages/Location";
+import Achievements from "./pages/Achievements";
+import FarmerHelp from "./pages/FarmerHelp";
 import NotFound from "./pages/NotFound";
 
 function App() {
@@ -20,7 +31,19 @@ function App() {
             <main className="flex-1">
               <Routes>
                 <Route path="/" element={<Home />} />
-                
+                <Route path="/about" element={<About />} />
+                <Route path="/team" element={<Team />} />
+                <Route path="/technology" element={<Technology />} />
+                <Route path="/success-stories" element={<SuccessStories />} />
+                <Route path="/soil-analysis" element={<SoilAnalysis />} />
+                <Route path="/chatbot" element={<Chatbot />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/location" element={<Location />} />
+                <Route path="/achievements" element={<Achievements />} />
+                <Route path="/farmer-help" element={<FarmerHelp />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
                 <Route path="*" element={<NotFound />} />
               </Routes>
             </main>
